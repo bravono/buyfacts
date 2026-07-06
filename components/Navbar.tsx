550cf4e20926d6e54f571799432b671547e8e393
Ahbideen Yusuf <ahbideeny@gmail.com>
2026-07-06 14:16:07 +0100

    Add BuyFacts logo to the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -32,20 +32,7 @@ export default function Navbar() {
     <header className={`${styles.header} ${isScrolled ? styles.scrolled : ""}`}>
       <div className={styles.container}>
         <a href="#hero" className={styles.logo} id="nav-logo-link">
-          <svg className={styles.logoSvg} viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-            {/* White rounded square card */}
-            <rect x="6" y="6" width="88" height="88" rx="12" fill="white" />
-            {/* Thick Orange outer border */}
-            <rect x="6" y="6" width="88" height="88" rx="12" stroke="#ff9900" strokeWidth="12" fill="none" />
-            
-            {/* Orange stylized 'F' monogram component */}
-            <path d="M24 24H50V34H36V46H48V56H36V76H24V24Z" fill="#ff9900" />
-            
-            {/* Deep Blue stylized 'B' / '3' monogram component */}
-            <path d="M50 24H68C74 24 78 28 78 34C78 40 74 44 68 44H50V24Z" fill="#00507b" />
-            <path d="M50 44H70C76 44 80 48 80 55C80 62 76 76 70 76H50V44Z" fill="#00507b" />
-            <rect x="42" y="24" width="12" height="52" fill="#00507b" />
-          </svg>
+         <img src="/logo.png" alt="BuyFacts Logo" style={{height: "50px"}}/>
           <div className={styles.logoTextWrapper}>
             <span className={styles.logoText}>
               Buy<span className={styles.logoHighlight}>Facts</span>
